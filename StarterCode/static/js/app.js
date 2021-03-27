310f7590236e00e319ffa888a43d27036ac74b3f
Keri <khennenf@yahoo.com>
2021-03-26 21:44:51 -0400

    updates

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,11 +1,1 @@
 
-// Fetch the JSON data and console log it
-// d3.json(json_data).then(function(data) {
-//     console.log(data);
-//   });
-  
-console.log(json_data)
-
-//   // Promise Pending
-//   const dataPromise = d3.json(url);
-//   console.log("Data Promise: ", dataPromise);
