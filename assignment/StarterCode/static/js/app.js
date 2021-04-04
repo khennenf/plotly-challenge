2af4d6e883132022f799188480a1f16288aae779
Keri <khennenf@yahoo.com>
2021-04-04 11:29:10 -0400

    changes

--- assignment/StarterCode/static/js/app.js
+++ assignment/StarterCode/static/js/app.js
@@ -6,13 +6,17 @@ d3.json("samples.json").then((dataFile) => {
     var singleSamples = dataFile.samples[0]
     var singleSamplesOtuIds = dataFile.samples[0].otu_ids.slice(0, 10)
     var singleSamplesOtuLabels = dataFile.samples[0].otu_labels.slice(0, 10)
-    var singleSamplesSampleValues = dataFile.samples[0].sample_values.slice(0, 10)
+    var singleSamplesSampleValues = dataFile.samples[0].sample_values.slice(0, 11)
+    
+    var sortedSamples =singleSamplesOtuIds.sort((a, b) => b.otu_ids - a.otu_ids)
+
 
     // console.log(singleMetadata)
     // console.log(singleSamples)
-    console.log(singleSamplesOtuIds)
-    console.log(singleSamplesOtuLabels)
-    console.log(singleSamplesSampleValues)
+    // console.log(singleSamplesOtuIds)
+    // console.log(singleSamplesOtuLabels)
+    // console.log(singleSamplesSampleValues)
+    console.log(sortedSamples)
 
     // function buildPlot(){
     //         d3.json("samples.json").then((dataFile) => {
@@ -22,9 +26,20 @@ d3.json("samples.json").then((dataFile) => {
     //         var singleSamplesOtuLabels = dataFile.samples[0].otu_labels
 
 
+    var trace = {
+        x: singleSamplesOtuIds,
+        y:singleSamplesSampleValues,
+        type: "bar",
+        orientation: "h"
+    }
 
+    // var layout = {
+        
+    // }
 
-    
+    var plotData = [trace]
+
+    Plotly.newPlot("bar", plotData);
 
 })
 
