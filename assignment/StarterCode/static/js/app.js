6d6cb8e014206d00eba377ba4fffca951b659a41
Keri <khennenf@yahoo.com>
2021-04-04 23:23:22 -0400

    adding options to dropdown

--- assignment/StarterCode/static/js/app.js
+++ assignment/StarterCode/static/js/app.js
@@ -1,60 +1,36 @@
+d3.select('#selDataset').on("change", getData)
+console.log(testSubId);
+
+var testSubId = 
+
+var option = document.createElement('option');
+option.text = '940'
+option.value = 0
+document.querySelector('#selDataset').add(option, 0)
+
+
+
+
+buildPlot(testSubId)
+
+
+function buildPlot() {
+
 d3.json("samples.json").then((dataFile) => {
-    // console.log(dataFile.metadata[0])
-    // console.log(dataFile.samples[0])
+    console.log(dataFile.metadata)
 
-    // var dataFor940 = dataFile.samples.slice(0,10)
+    var i = testSubId
 
-    // console.log(dataFor940[0])
-
- 
-
+    var singleSamplesOtuIds = dataFile.samples[i].otu_ids.slice(0, 10)
+    var singleSamplesOtuLabels = dataFile.samples[i].otu_labels.slice(0, 10)
+    var singleSamplesSampleValues = dataFile.samples[i].sample_values.slice(0, 10)
 
     
-
-
-    // var singleMetadata = dataFile.metadata[0]
-    // var singleSamples = dataFile.samples[0]
-    var singleSamplesOtuIds = dataFile.samples[10].otu_ids.slice(0, 10)
-    var singleSamplesOtuLabels = dataFile.samples[10].otu_labels.slice(0, 10)
-    var singleSamplesSampleValues = dataFile.samples[10].sample_values.slice(0, 10)
-    
-    // singleSamplesSampleValues.sort(function compareFunction(firstNum, secondNum) {
-    //     return secondNum - firstNum;
-    // });
-
-    // console.log(singleSamplesSampleValues)
-
-    // var singleSamplesSampleValuesTop10 = dataFile.samples[0].sample_values.slice(0, 10)
-
-    // console.log(singleSamplesSampleValuesTop10);
-
     var textOTU = 'OTU'
 
-   var textOtuIds = ' ';
-   singleSamplesOtuIds.forEach(id => textOtuIds += 'OTU' + ' ' + id + ',' )        
-   var labeledIds = textOtuIds.split(',', 10)
-   
-
-// for (id in singleSamplesOtuIds) {
-//     textOtuIds += textOTU + ' ' + id
-// }
-
-    // console.log(textOtuIds)
-
-    // // console.log(singleMetadata)
-    // // console.log(singleSamples)
-    // // console.log(singleSamplesOtuIds)
-    // // console.log(singleSamplesOtuLabels)
-    // // console.log(singleSamplesSampleValues)
-
-
-    // // function buildPlot(){
-    // //         d3.json("samples.json").then((dataFile) => {
-    // //         var singleMetadata = dataFile.metadata[0]
-    // //         var singleSamples = dataFile.samples[0]
-    // //         var singleSamplesOtuIds = dataFile.samples[0].otu_ids
-    // //         var singleSamplesOtuLabels = dataFile.samples[0].otu_labels
-
+    var textOtuIds = ' ';
+    singleSamplesOtuIds.forEach(id => textOtuIds += 'OTU' + ' ' + id + ',' )        
+    var labeledIds = textOtuIds.split(',', 10)
 
     var trace = {
         x: singleSamplesSampleValues,
@@ -64,28 +40,43 @@ d3.json("samples.json").then((dataFile) => {
         orientation: "h"
     }
 
-    // // var layout = {
-        
-    // // }
-
-    console.log(singleSamplesSampleValues)
-    console.log(singleSamplesOtuIds)
-    var plotData = [trace]
+     var plotData = [trace]
 
     Plotly.newPlot("bar", plotData);
 
-
-
-// function buildPlot(){
-//     d3.json("samples.json").then((dataFile) => {
-//         var id = dataFile.samples.id;
-//         var otu_ids = dataFile.samples.otu_ids;
-//         var sample_values = dataFile.samples.sample_values;
-//         var otu_lables = dataFile.samples.otu_lables;
-
-//         console.log(dataFile.id)
-
-// });
-// }
     })
-// buildPlot();
+
+}
+buildPlot();
+
+
+    // // console.log(singleMetadata)
+    // // console.log(singleSamples)
+    // // console.log(singleSamplesOtuIds)
+    // // console.log(singleSamplesOtuLabels)
+    // // console.log(singleSamplesSampleValues)
+    
+    // console.log(singleSamplesSampleValues)
+    // console.log(singleSamplesOtuIds)
+   
+        // console.log(dataFile.metadata[0])
+    // console.log(dataFile.samples[0])
+
+    // var dataFor940 = dataFile.samples.slice(0,10)
+
+    // console.log(dataFor940[0])
+
+    
+    // var singleMetadata = dataFile.metadata[0]
+    // var singleSamples = dataFile.samples[0]
+
+   
+    // singleSamplesSampleValues.sort(function compareFunction(firstNum, secondNum) {
+    //     return secondNum - firstNum;
+    // });
+
+    // console.log(singleSamplesSampleValues)
+
+    // var singleSamplesSampleValuesTop10 = dataFile.samples[0].sample_values.slice(0, 10)
+
+    // console.log(singleSamplesSampleValuesTop10);
