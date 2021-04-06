f6e76f7c0eeafb3f27f01612b1ad4468092204bb
Keri <khennenf@yahoo.com>
2021-04-06 14:20:28 -0400

    switch to panel

--- assignment/StarterCode/static/js/app.js
+++ assignment/StarterCode/static/js/app.js
@@ -1,53 +1,63 @@
-//Add options to
-// var option = document.createElement('option');
-// option.text = '940'
-// option.value = 0
-// document.querySelector('#selDataset').add(option, 0)
-
-// buildPlot(testSubId)
-
-// function buildPlot() 
 
 
-d3.json("samples.json").then((dataFile) => {
+var e = document.getElementById("elementId");
+
+var value = e.options[e.selectedIndex].value;
+
+var text = e.options[e.selectedIndex].text;
+
+
+
+// //Add options to
+// // var option = document.createElement('option');
+// // option.text = '940'
+// // option.value = 0
+// // document.querySelector('#selDataset').add(option, 0)
+
+// // buildPlot(testSubId)
+
+// // function buildPlot() 
+
+
+// d3.json("samples.json").then((dataFile) => {
     
-    var i = 10
-    var singleSamplesOtuIds = dataFile.samples[i].otu_ids.slice(0, 10)
-    var singleSamplesOtuLabels = dataFile.samples[i].otu_labels.slice(0, 10)
-    var singleSamplesSampleValues = dataFile.samples[i].sample_values.slice(0, 10)
-    console.log(dataFile.samples)
-    // console.log(singleSamplesOtuIds)
+//     var i = 10
+//     var singleSamplesOtuIds = dataFile.samples[i].otu_ids.slice(0, 10)
+//     var singleSamplesOtuLabels = dataFile.samples[i].otu_labels.slice(0, 10)
+//     var singleSamplesSampleValues = dataFile.samples[i].sample_values.slice(0, 10)
+//     console.log(dataFile.samples)
+//     // console.log(singleSamplesOtuIds)
    
-    var textOTU = 'OTU'
-    var textOtuIds = ' ';
-    singleSamplesOtuIds.forEach(id => textOtuIds += 'OTU' + ' ' + id + ',' )        
-    var labeledIds = textOtuIds.split(',', 10)
+//     var textOTU = 'OTU'
+//     var textOtuIds = ' ';
+//     singleSamplesOtuIds.forEach(id => textOtuIds += 'OTU' + ' ' + id + ',' )        
+//     var labeledIds = textOtuIds.split(',', 10)
 
-    // d3.select('#selDataset').on("change", optionChanged)
+//     // d3.select('#selDataset').on("change", optionChanged)
 
 
-    // function optionChanged() {
-    //     var dropdownMenu = d3.select('#selDataset');
-    //     var dataset = dropdownMenu.property('value');
-    //     var data = []
+//     // function optionChanged() {
+//     //     var dropdownMenu = d3.select('#selDataset');
+//     //     var dataset = dropdownMenu.property('value');
+//     //     var data = []
 
-    // if (dataset == 0) {
-    //     dataset = testSubId
-    // }
+//     // if (dataset == 0) {
+//     //     dataset = testSubId
+//     // }
 
-    var trace = {
-        x: singleSamplesSampleValues,
-        y: labeledIds,
-        text: singleSamplesOtuLabels,
-        type: "bar",
-        orientation: "h"
-    }
+//     var trace = {
+//         x: singleSamplesSampleValues,
+//         y: labeledIds,
+//         text: singleSamplesOtuLabels,
+//         type: "bar",
+//         orientation: "h"
+//     }
 
-    var plotData = [trace]
+//     var plotData = [trace]
 
-    Plotly.newPlot("bar", plotData);
+//     Plotly.newPlot("bar", plotData);
 
     
-})
+// })
 
-// buildPlot();
+// // buildPlot();
--- assignment/StarterCode/static/js/app_copy.js
+++ assignment/StarterCode/static/js/app_copy.js
@@ -1,30 +1,57 @@
-// function handleSubmit() {
+//Add options to
+var option = document.createElement('option');
+option.text = '941'
+option.value = 1
+document.querySelector('#selDataset').add(option, 0)
 
-//     d3.event.preventDefault();
+var option = document.createElement('option');
+option.text = '940'
+option.value = 0
+document.querySelector('#selDataset').add(option, 0)
 
+
+
+
+function handleSubmit() {
+
+    d3.event.preventDefault();
+}
     // var testSubId = d3.select("#selDataset").node().value;
     // console.log(testSubId);
 
-function buildPlot(idValue) {
+    d3.selectAll("body").on("change", optionChanged);
+
+    function optionChanged() {
+        var dropdownMenu = d3.selectAll("#selDataset").node();
+    
+        var dropdownMenuID = dropdownMenu.id;
+    
+        var idValue = dropdownMenu.value;
+        // console.log(idValue)
+
+
+function buildPlot() {
 
     d3.json("samples.json").then((dataFile) => {
         console.log(dataFile)
-    
-       i = idValue
+
+        var i = idValue
        
         var nameSection = dataFile.names
-        console.log(nameSection)
+        // console.log(nameSection)
 
         var metadataSection = dataFile.metadata
-        console.log(metadataSection)
+        // console.log(metadataSection)
 
         var samplesById = dataFile.samples.map(row => row)
         var sampleIdOtuId = samplesById[i].otu_ids
-        console.log(sampleIdOtuId.slice(0,10))
+        // console.log(sampleIdOtuId.slice(0,10))
         var sampleByIdSampleValues = samplesById[i].sample_values
-        console.log(sampleByIdSampleValues)
+        // console.log(sampleByIdSampleValues)
         var sampleId  = samplesById[i].id
-        console.log(sampleId)
+        // console.log(sampleId)
+        var allIds = samplesById.id
+        // console.log(allIds)
 
         var trace = {
         type: "bar",
@@ -51,4 +78,10 @@ function buildPlot(idValue) {
     )
 }
 
- buildPlot(15)
+buildPlot()
+
+console.log(data)
+
+}
+
+
