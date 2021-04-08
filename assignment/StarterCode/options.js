0392c085bd6cb4357e2f8f88425400c93376f679
Keri <khennenf@yahoo.com>
2021-04-08 19:50:07 -0400

    changes

---
+++ assignment/StarterCode/options.js
@@ -0,0 +1,478 @@
+var option = document.createElement('option');
+option.text = 940
+option.value = 0
+document.querySelector('#selDataset').add(option, 0)
+
+var option = document.createElement('option');
+option.text = 941
+option.value = 1
+document.querySelector('#selDataset').add(option, 0)
+
+var option = document.createElement('option');
+option.text = 943
+option.value = 2
+document.querySelector('#selDataset').add(option, 0)
+
+var option = document.createElement('option');
+option.text = 944
+option.value = 3
+document.querySelector('#selDataset').add(option, 0)
+
+var option = document.createElement('option');
+option.text = 945
+option.value = 4
+document.querySelector('#selDataset').add(option, 0)
+
+var option = document.createElement('option');
+option.text = 946
+option.value = 5
+document.querySelector('#selDataset').add(option, 0)
+
+var option = document.createElement('option');
+option.text = 947
+option.value = 6
+document.querySelector('#selDataset').add(option, 0)
+
+var option = document.createElement('option');
+option.text = 948
+option.value = 7
+document.querySelector('#selDataset').add(option, 0)
+
+var option = document.createElement('option');
+option.text = 949
+option.value = 8
+document.querySelector('#selDataset').add(option, 0)
+
+var option = document.createElement('option');
+option.text = 950
+option.value = 9
+document.querySelector('#selDataset').add(option, 0)
+
+var option = document.createElement('option');
+option.text = 952
+option.value = 10
+document.querySelector('#selDataset').add(option, 0)
+
+var option = document.createElement('option');
+option.text = 953
+option.value = 11
+document.querySelector('#selDataset').add(option, 0)
+
+var option = document.createElement('option');
+option.text = 954
+option.value = 12
+document.querySelector('#selDataset').add(option, 0)
+
+var option = document.createElement('option');
+option.text = 955
+option.value = 13
+document.querySelector('#selDataset').add(option, 0)
+
+var option = document.createElement('option');
+option.text = 956
+option.value = 14
+document.querySelector('#selDataset').add(option, 0)
+
+var option = document.createElement('option');
+option.text = 958
+option.value = 15
+document.querySelector('#selDataset').add(option, 0)
+
+var option = document.createElement('option');
+option.text = 959
+option.value = 16
+document.querySelector('#selDataset').add(option, 0)
+
+var option = document.createElement('option');
+option.text = 960
+option.value = 17
+document.querySelector('#selDataset').add(option, 0)
+
+var option = document.createElement('option');
+option.text = 961
+option.value = 18
+document.querySelector('#selDataset').add(option, 0)
+
+var option = document.createElement('option');
+option.text = 962
+option.value = 19
+document.querySelector('#selDataset').add(option, 0)
+
+var option = document.createElement('option');
+option.text = 963
+option.value = 20
+document.querySelector('#selDataset').add(option, 0)
+
+var option = document.createElement('option');
+option.text = 964
+option.value = 21
+document.querySelector('#selDataset').add(option, 0)
+
+var option = document.createElement('option');
+option.text = 966
+option.value = 22
+document.querySelector('#selDataset').add(option, 0)
+
+var option = document.createElement('option');
+option.text = 967
+option.value = 23
+document.querySelector('#selDataset').add(option, 0)
+
+var option = document.createElement('option');
+option.text = 968
+option.value = 24
+document.querySelector('#selDataset').add(option, 0)
+
+var option = document.createElement('option');
+option.text = 969
+option.value = 25
+document.querySelector('#selDataset').add(option, 0)
+
+var option = document.createElement('option');
+option.text = 940
+option.value = 26
+document.querySelector('#selDataset').add(option, 0)
+
+var option = document.createElement('option');
+option.text = 971
+option.value = 27
+document.querySelector('#selDataset').add(option, 0)
+
+var option = document.createElement('option');
+option.text = 972
+option.value = 28
+document.querySelector('#selDataset').add(option, 0)
+
+var option = document.createElement('option');
+option.text = 973
+option.value = 29
+document.querySelector('#selDataset').add(option, 0)
+
+var option = document.createElement('option');
+option.text = 974
+option.value = 30
+document.querySelector('#selDataset').add(option, 0)
+
+var option = document.createElement('option');
+option.text = 975
+option.value = 31
+document.querySelector('#selDataset').add(option, 0)
+
+var option = document.createElement('option');
+option.text = 978
+option.value = 32
+document.querySelector('#selDataset').add(option, 0)
+
+var option = document.createElement('option');
+option.text = 1233
+option.value = 33
+document.querySelector('#selDataset').add(option, 0)
+
+var option = document.createElement('option');
+option.text = 1234
+option.value = 34
+document.querySelector('#selDataset').add(option, 0)
+
+var option = document.createElement('option');
+option.text = 1235
+option.value = 35
+document.querySelector('#selDataset').add(option, 0)
+
+var option = document.createElement('option');
+option.text = 1236
+option.value = 36
+document.querySelector('#selDataset').add(option, 0)
+
+var option = document.createElement('option');
+option.text = 1237
+option.value = 37
+document.querySelector('#selDataset').add(option, 0)
+
+var option = document.createElement('option');
+option.text = 1238
+option.value = 38
+document.querySelector('#selDataset').add(option, 0)
+
+var option = document.createElement('option');
+option.text = 1242
+option.value = 39
+document.querySelector('#selDataset').add(option, 0)
+
+var option = document.createElement('option');
+option.text = 1243
+option.value = 40
+document.querySelector('#selDataset').add(option, 0)
+
+var option = document.createElement('option');
+option.text = 1246
+option.value = 41
+document.querySelector('#selDataset').add(option, 0)
+
+var option = document.createElement('option');
+option.text = 1253
+option.value = 42
+document.querySelector('#selDataset').add(option, 0)
+
+var option = document.createElement('option');
+option.text = 1254
+option.value = 43
+document.querySelector('#selDataset').add(option, 0)
+
+var option = document.createElement('option');
+option.text = 1258
+option.value = 44
+document.querySelector('#selDataset').add(option, 0)
+
+var option = document.createElement('option');
+option.text = 1259
+option.value = 45
+document.querySelector('#selDataset').add(option, 0)
+
+var option = document.createElement('option');
+option.text = 1260
+option.value = 46
+document.querySelector('#selDataset').add(option, 0)
+
+var option = document.createElement('option');
+option.text = 1264
+option.value = 47
+document.querySelector('#selDataset').add(option, 0)
+
+var option = document.createElement('option');
+option.text = 1265
+option.value = 48
+document.querySelector('#selDataset').add(option, 0)
+
+var option = document.createElement('option');
+option.text = 1273
+option.value = 49
+document.querySelector('#selDataset').add(option, 0)
+
+var option = document.createElement('option');
+option.text = 1275
+option.value = 50
+document.querySelector('#selDataset').add(option, 0)
+
+var option = document.createElement('option');
+option.text = 1276
+option.value = 51
+document.querySelector('#selDataset').add(option, 0)
+
+var option = document.createElement('option');
+option.text = 1277
+option.value = 52
+document.querySelector('#selDataset').add(option, 0)
+
+var option = document.createElement('option');
+option.text = 1278
+option.value = 53
+document.querySelector('#selDataset').add(option, 0)
+
+var option = document.createElement('option');
+option.text = 1279
+option.value = 54
+document.querySelector('#selDataset').add(option, 0)
+
+var option = document.createElement('option');
+option.text = 1280
+option.value = 55
+document.querySelector('#selDataset').add(option, 0)
+
+var option = document.createElement('option');
+option.text = 1281
+option.value = 56
+document.querySelector('#selDataset').add(option, 0)
+
+var option = document.createElement('option');
+option.text = 1282
+option.value = 57
+document.querySelector('#selDataset').add(option, 0)
+
+var option = document.createElement('option');
+option.text = 1283
+option.value = 58
+document.querySelector('#selDataset').add(option, 0)
+
+var option = document.createElement('option');
+option.text = 1284
+option.value = 59
+document.querySelector('#selDataset').add(option, 0)
+
+var option = document.createElement('option');
+option.text = 1285
+option.value = 60
+document.querySelector('#selDataset').add(option, 0)
+
+var option = document.createElement('option');
+option.text = 1286
+option.value = 61
+document.querySelector('#selDataset').add(option, 0)
+
+var option = document.createElement('option');
+option.text = 1287
+option.value = 62
+document.querySelector('#selDataset').add(option, 0)
+
+var option = document.createElement('option');
+option.text = 1288
+option.value = 63
+document.querySelector('#selDataset').add(option, 0)
+
+var option = document.createElement('option');
+option.text = 1289
+option.value = 64
+document.querySelector('#selDataset').add(option, 0)
+
+var option = document.createElement('option');
+option.text = 1290
+option.value = 65
+document.querySelector('#selDataset').add(option, 0)
+
+var option = document.createElement('option');
+option.text = 1291
+option.value = 66
+document.querySelector('#selDataset').add(option, 0)
+
+var option = document.createElement('option');
+option.text = 1292
+option.value = 67
+document.querySelector('#selDataset').add(option, 0)
+
+var option = document.createElement('option');
+option.text = 1293
+option.value = 68
+document.querySelector('#selDataset').add(option, 0)
+
+var option = document.createElement('option');
+option.text = 1294
+option.value = 69
+document.querySelector('#selDataset').add(option, 0)
+
+var option = document.createElement('option');
+option.text = 
+option.value = 
+document.querySelector('#selDataset').add(option, 0)
+
+var option = document.createElement('option');
+option.text = 
+option.value = 
+document.querySelector('#selDataset').add(option, 0)
+
+var option = document.createElement('option');
+option.text = 
+option.value = 
+document.querySelector('#selDataset').add(option, 0)
+
+var option = document.createElement('option');
+option.text = 
+option.value = 
+document.querySelector('#selDataset').add(option, 0)
+
+var option = document.createElement('option');
+option.text = 
+option.value = 
+document.querySelector('#selDataset').add(option, 0)
+
+var option = document.createElement('option');
+option.text = 
+option.value = 
+document.querySelector('#selDataset').add(option, 0)
+
+var option = document.createElement('option');
+option.text = 
+option.value = 
+document.querySelector('#selDataset').add(option, 0)
+
+var option = document.createElement('option');
+option.text = 
+option.value = 
+document.querySelector('#selDataset').add(option, 0)
+
+var option = document.createElement('option');
+option.text = 
+option.value = 
+document.querySelector('#selDataset').add(option, 0)
+
+var option = document.createElement('option');
+option.text = 
+option.value = 
+document.querySelector('#selDataset').add(option, 0)
+
+var option = document.createElement('option');
+option.text = 
+option.value = 
+document.querySelector('#selDataset').add(option, 0)
+
+var option = document.createElement('option');
+option.text = 
+option.value = 
+document.querySelector('#selDataset').add(option, 0)
+
+var option = document.createElement('option');
+option.text = 
+option.value = 
+document.querySelector('#selDataset').add(option, 0)
+
+var option = document.createElement('option');
+option.text = 
+option.value = 
+document.querySelector('#selDataset').add(option, 0)
+
+var option = document.createElement('option');
+option.text = 
+option.value = 
+document.querySelector('#selDataset').add(option, 0)
+
+var option = document.createElement('option');
+option.text = 
+option.value = 
+document.querySelector('#selDataset').add(option, 0)
+
+var option = document.createElement('option');
+option.text = 
+option.value = 
+document.querySelector('#selDataset').add(option, 0)
+
+var option = document.createElement('option');
+option.text = 
+option.value = 
+document.querySelector('#selDataset').add(option, 0)
+
+var option = document.createElement('option');
+option.text = 
+option.value = 
+document.querySelector('#selDataset').add(option, 0)
+
+var option = document.createElement('option');
+option.text = 
+option.value = 
+document.querySelector('#selDataset').add(option, 0)
+
+var option = document.createElement('option');
+option.text = 
+option.value = 
+document.querySelector('#selDataset').add(option, 0)
+
+var option = document.createElement('option');
+option.text = 
+option.value = 
+document.querySelector('#selDataset').add(option, 0)
+
+var option = document.createElement('option');
+option.text = 
+option.value = 
+document.querySelector('#selDataset').add(option, 0)
+
+var option = document.createElement('option');
+option.text = 
+option.value = 
+document.querySelector('#selDataset').add(option, 0)
+
+var option = document.createElement('option');
+option.text = 
+option.value = 
+document.querySelector('#selDataset').add(option, 0)
+
+
+
+
--- assignment/StarterCode/static/js/app_copy_index_find.js
+++ assignment/StarterCode/static/js/app_copy_index_find.js
@@ -3,9 +3,24 @@ option.text = '941'
 option.value = 1
 document.querySelector('#selDataset').add(option, 0)
 
+
+// var select = document.getElementById("selDataset"); 
+// var options = ["1", "2", "3", "4", "5"]; 
+
+// for(var i = 0; i < options.length; i++) {
+//     var opt = options[i];
+//     var el = document.createElement("option");
+//     el.textContent = opt;
+//     el.value = opt;
+//     select.appendChild(el);
+// }
+
+
 d3.json("samples.json").then((dataFile) => {
 
 var namesArray = dataFile.names
+
+console.log(namesArray)
 
 var option = document.createElement('option');
 
@@ -18,7 +33,53 @@ function myFunction(key, value) {
 
 namesArray.forEach(myFunction)
 
+
+
+// var select = document.getElementById("selDataset"); 
+var options = namesArray.keys()
+var optoinsIndex = namesArray.values()
+
+for (x of options) {
+    console.log(x)
+}
+
+for(var i = 0; i < options.length; i++) {
+    var opt = options[i];
+    var el = document.createElement("option");
+    el.textContent = key
+    el.value = opt(keys)
+    select.appendChild(el);
+}
+
  
+
+  let dateDropdown = document.getElementById('selDataset'); 
+       
+  let currentYear = options
+  let earliestYear = 1970;     
+  while (currentYear >= earliestYear) {      
+    let dateOption = document.createElement('option');          
+    dateOption.text = currentYear  
+    dateOption.value = currentYear;        
+    dateDropdown.add(dateOption);      
+    currentYear -= 1;    
+  }
+
+// let dateDropdown = document.getElementById('selDataset'); 
+       
+// let currentYear = new Date().getFullYear();    
+// let currentYearDisplay = ["1", "2", "3"];   
+// let earliestYear = 2019;     
+// while (currentYear >= earliestYear) {      
+//   let dateOption = document.createElement('option');          
+//   dateOption.text = currentYearDisplay
+//   dateOption.value = currentYear;        
+//   dateDropdown.add(dateOption);      
+//   currentYear -= 1;    
+// }
+
+
+
 function handleSubmit() {
 
     d3.event.preventDefault();
@@ -33,7 +94,11 @@ function handleSubmit() {
         d3.json("samples.json").then((dataFile) => {
         var namesArray = dataFile.names
         subDisplayValues = Object.values(namesArray)
+        subDisplayValuesKeys = Object.keys(namesArray)
         var i = idValue
+
+        console.log(subDisplayValues)
+        console.log(subDisplayValuesKeys)
 
 })
 
